5a75377ca0ae21f5d1961cf988ea5a08cc746cf5
Abraham Coombs <coombsab@gmail.com>
2022-09-22 17:04:51 -0600

    DB setup

---
+++ client/app/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'bcw-abraham.us.auth0.com'
+export const audience = 'https://bcw-projects.com'
+export const clientId = 'VqYWGhhwW8BFtcj48GHzxSX4s068q0zY'

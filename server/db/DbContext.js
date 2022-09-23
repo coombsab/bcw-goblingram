e889d2f0597787d97b615443aed3216d98545362
Samuel Wilkins <samuel.wilkins.official@gmail.com>
2022-09-23 12:31:18 -0600

    Remove Goblin Garbage

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,12 +1,10 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
-import { GoblinSchema } from '../models/Goblin.js';
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
-  Goblins = mongoose.model('Goblin', GoblinSchema)
 }
 
 export const dbContext = new DbContext()

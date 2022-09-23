f0902deec459c7ba63a688f68d82ef66bb6ad010
molly-nettleton <mollyelainen@gmail.com>
2022-09-23 17:12:51 -0600

    Voter Backend Setup

---
+++ server/models/PostUpVoter.js
@@ -0,0 +1,23 @@
+import { Schema } from "mongoose";
+const ObjectId = Schema.Types.ObjectId
+export const PostUpVoterSchema = new Schema({
+  goblinId: { type: ObjectId, required: true, ref: 'Account' },
+  postId: { type: ObjectId, required: true, ref: 'Post' },
+},)
+
+
+PostUpVoterSchema.virtual('upVoter', {
+  localField: 'upVoterId',
+  foreignField: '_id',
+  justOne: true,
+  ref: 'Account'
+})
+
+PostUpVoterSchema.virtual('post', {
+  localField: 'postId',
+  foreignField: '_id',
+  justOne: true,
+  ref: 'Post'
+})
+
+PostUpVoterSchema.index({ upVoterId: 1, postId: 1 }, { unique: true })

1dbb8fbfb986f39680a3d7c05b1d8f735146c62f
Jacob Neitzell <107662321+JacobNeitzell@users.noreply.github.com>
2022-09-23 15:15:52 -0600

    Front end Get and create post done

--- client/app/Controllers/PostsController.js
+++ client/app/Controllers/PostsController.js
@@ -1,5 +1,7 @@
+import bootstrap from "bootstrap"
 import { appState } from "../AppState.js"
 import { postsService } from "../Services/PostsService.js"
+import { getFormData } from "../Utils/FormHandler.js"
 import { Pop } from "../Utils/Pop.js"
 import { setHTML } from "../Utils/Writer.js"
 
@@ -11,16 +13,37 @@ function _drawPosts() {
 
 export class PostsController {
   constructor() {
-    
+
   }
 
   async getPosts() {
     try {
       await postsService.getPosts()
     }
-    catch(error) {
+    catch (error) {
       console.error('[functionName]', error)
       Pop.error(error.message)
     }
   }
+
+  async createPosts() {
+    try {
+      // @ts-ignore
+      window.event.preventDefault()
+      // @ts-ignore
+      const form = window.event.target
+      const formData = getFormData(form)
+      await postsService.createPosts(formData)
+      // @ts-ignore
+      form.reset()
+      const modal = bootstrap.Modal.getOrCreateInstance('addPostModal')
+      modal.hide()
+    } catch (error) {
+      Pop.error(error.message)
+    }
+  }
+
+  //TODO edit and delete 
+
 }
+
--- client/app/Services/PostsService.js
+++ client/app/Services/PostsService.js
@@ -1,11 +1,25 @@
+import { Server } from "socket.io"
+import { appState } from "../AppState.js";
+import { Post } from "../Models/Post.js";
+import { server } from "./AxiosService.js"
+
 class PostsService {
   async getPosts() {
-    
+    const res = await server.get('api/gg/posts')
+    console.log(res.data);
+    appState.posts = res.data.map(p => new Post(p))
+
+
+  }
+  async createPosts(formData) {
+    const res = await server.post('api/gg/posts', formData)
+    console.log(res.data)
+    appState.posts = [...appState.posts, new Post(res.data)]
+
   }
 
-  constructor() {
 
-  }
+
 }
 
 export const postsService = new PostsService()

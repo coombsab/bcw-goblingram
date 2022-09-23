23bdb2dec1093db5e46175229ee007d70d215d72
Samuel Wilkins <samuel.wilkins.official@gmail.com>
2022-09-23 17:03:37 -0600

    Location is now fixed!

--- client/app/Models/Post.js
+++ client/app/Models/Post.js
@@ -16,7 +16,7 @@ export class Post {
           <div class="card">
             <div class="card-body d-flex justify-content-between">
               <h4 class="text-center" id="post-title">${this.title}</h4>
-              <span id="post-date">${this.date}</span>
+              <span id="post-date">${this.location}</span>
               <span id="post-heart" class="fs-3">💖</span>
               <span id="post-upvotes" class="fs-3">⬆️</span><span>20</span>
               <span id="post-downvotes" class="fs-3">⬇️</span><span>500</span>

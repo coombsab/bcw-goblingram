25cba7f9d4f4dd5a5f2284f7e181c55c8d9b2600
Jacob Neitzell <107662321+JacobNeitzell@users.noreply.github.com>
2022-09-23 11:43:14 -0600

    Comments model Back end  done

---
+++ server/models/Comment.js
@@ -0,0 +1,37 @@
+import { Schema } from 'mongoose';
+const ObjectId = Schema.Types.ObjectId
+
+
+export const CommentSchema = new Schema({
+
+  goblinId: { type: ObjectId, required: true, ref: 'Account' },
+  postId: { type: ObjectId, required: true, ref: 'Post' },
+  description: { type: String, required: true },
+  voterId: { type: ObjectId, required: true, ref: 'Account' }
+}, {
+  timestamps: true, toJSON: { virtuals: true }
+})
+
+CommentSchema.virtual('goblin', {
+  localField: 'goblinId',
+  foreignField: '_id',
+  justOne: true,
+  ref: 'Account'
+})
+
+CommentSchema.virtual('postId', {
+  localField: 'postId',
+  foreignField: '_id',
+  justOne: true,
+  ref: 'Post'
+})
+
+CommentSchema.virtual('voter', {
+  localField: 'voterId',
+  foreignField: '_id',
+  justOne: true,
+  ref: 'Voter'
+})
+
+
+

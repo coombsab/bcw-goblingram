b0b34f04825ec1477996e131e1b055aa5d8c1c65
molly-nettleton <mollyelainen@gmail.com>
2022-09-23 09:12:52 -0600

    test

--- client/app/App.js
+++ client/app/App.js
@@ -1,9 +1,9 @@
 import { AuthController } from './Controllers/AuthController.js';
-import { ValuesController } from './Controllers/ValuesController.js';
+// import { ValuesController } from './Controllers/ValuesController.js';
 
 class App {
   authController = new AuthController();
-  valuesController = new ValuesController();
+  // valuesController = new ValuesController();
 }
 
 // @ts-ignore

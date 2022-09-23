583f47ff8bba1108f8a83a5af1c770da842df9d7
Abraham Coombs <coombsab@gmail.com>
2022-09-23 10:56:57 -0600

    Created FE Post Model

---
+++ client/app/Models/Post.js
@@ -0,0 +1,9 @@
+export class Post {
+  constructor(data) {
+  this.title = data.title
+  this.imgUrl = data.imgUrl
+  this.location = data.location
+  this.goblinId = data.goblinId
+  this.tags = data.tags
+  }
+}

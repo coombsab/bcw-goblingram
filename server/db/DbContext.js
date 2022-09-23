d68e3f1134b57e2aeaafd66aeb8a58997fd5a4cb
Jacob Neitzell <107662321+JacobNeitzell@users.noreply.github.com>
2022-09-23 12:17:09 -0600

    Comments controller started

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
+import { CommentSchema } from "../models/Comment.js";
 import { GoblinSchema } from '../models/Goblin.js';
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
-  Goblins = mongoose.model('Goblin', GoblinSchema)
+  Goblins = mongoose.model('Goblin', GoblinSchema);
+  Comment = mongoose.model('Comment', CommentSchema)
 }
-
 export const dbContext = new DbContext()

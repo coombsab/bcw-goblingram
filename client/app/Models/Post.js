92f443abef23e649a4b19a5be080beb5206b64ff
molly-nettleton <mollyelainen@gmail.com>
2022-09-23 23:03:42 -0600

    Style Changes

--- client/app/Models/Post.js
+++ client/app/Models/Post.js
@@ -16,7 +16,7 @@ export class Post {
     return /*html*/ `
           <div class="card col-md-4 col-12">
           
-            <div class="card-header ">
+            <div class="card-header bg-dark text-light">
             <h4 class="text-center" id="post-title">${this.title}</h4>
             <div class="d-flex justify-content-between">
               <span id="post-date">${this.location}</span>
@@ -29,13 +29,13 @@ export class Post {
               src="${this.imgUrl}"
               alt="" class="img-fluid rounded-top" id="post-image">
             <div class="card-body text-center d-flex justify-content-between">
-              <button class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#addCommentModal" onclick="app.postsController.setActivePost('${this.id}')"
+              <button class="btn btn-secondary text-light" data-bs-toggle="modal" data-bs-target="#addCommentModal" onclick="app.postsController.setActivePost('${this.id}')"
               >Add comment
               </button>
-              <button class="btn btn-dark" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"
+              <button class="btn btn-secondary text-light" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"
                 aria-controls="offcanvasExample" onclick="app.commentsController.getComments('${this.id}')">View comments
               </button>
-              <button class="btn btn-dark">Edit</button>
+              <button class="btn btn-secondary text-light">Edit</button>
               // <button class="btn btn-danger" onclick="app.postsController.deletePost('${this.id}')">Delete</button>
             </div>
           </div>
